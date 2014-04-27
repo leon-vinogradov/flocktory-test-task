801f6c8074feec2f23b8d19d4f3b7799d14c35fc
Leonid Vinogradov <leon.vinogradov@gmail.com>
2014-04-27 22:50:32 +0400

    Processing min and max selected campaigns

--- flocktory.js
+++ flocktory.js
@@ -7,6 +7,8 @@
 var floctory = {
 	'config': {
 		metrics: ['offers', 'shares', 'landings', 'leads', 'purchases', 'friends'],
+		minCampaigns: 2,
+		maxCampaigns: 8,
 		margin: {
 			top: 120,
 			right: 10,
@@ -21,7 +23,7 @@ var floctory = {
 		var self = this;
 
 		d3.json('data.json', function(error, data) {
-			data = data.slice(10, 20); // Test with short data list
+			data = data.slice(10, 40); // Test with short data list
 
 			self.color = d3.scale.category10();
 
@@ -38,46 +40,65 @@ var floctory = {
 			self.stack = d3.layout.stack()
 				.values(function(d) { return d.values; });
 
-			self.chart = d3.select("#compare-chart").append("svg")
-				.attr("width", self.config.width)
-				.attr("height", self.config.height)
-				.attr("transform", "rotate(90," + self.config.width / 2 + "," + self.config.height / 2 + ")");
+			self.chart = d3.select('#compare-chart').append('svg')
+				.attr('width', self.config.width)
+				.attr('height', self.config.height)
+				.attr('transform', 'rotate(90,' + self.config.width / 2 + ',' + self.config.height / 2 + ')');
 
-			self.chartArea = self.chart.append("g")
-				.attr("transform", "translate(" + self.config.margin.left + "," + self.config.margin.top + ")");
-			self.chartYAxis = self.chart.append("g")
-				.attr("transform", "translate(" + self.config.margin.left + "," + self.config.margin.top + ")")
-				.attr("class", "axis");
+			self.chartArea = self.chart.append('g')
+				.attr('transform', 'translate(' + self.config.margin.left + ',' + self.config.margin.top + ')');
+			self.chartYAxis = self.chart.append('g')
+				.attr('transform', 'translate(' + self.config.margin.left + ',' + self.config.margin.top + ')')
+				.attr('class', 'axis');
 
 			self.area = d3.svg.area()
 				.x(function(d) { return self.x(d.x); })
 				.y0(function(d) { return self.y(d.y0); })
 				.y1(function(d) { return self.y(d.y0 + d.y); });
 
-			var listItem = d3.select("#campaign-list")
-				.selectAll("li")
+			var listItem = d3.select('#campaign-list')
+				.selectAll('li')
 					.data(data)
-				.enter().append("li").append("label");
+				.enter().append('li').append('label');
 
-			listItem.append("input").attr("type", "checkbox");
-			listItem.append("span").text(function(d) { return d.title; });
+			listItem.append('input').attr('type', 'checkbox');
+			listItem.append('span').text(function(d) { return d.title; });
 
-			listItem.on("change", function() {
-				var selectedCampaigns = d3.selectAll("#campaign-list input:checked"),
-					selectedCampaignsLength = selectedCampaigns[0].length;
-
-				if ((-1 < selectedCampaignsLength) && (selectedCampaignsLength < 8)) {
-					self.makeGraph(selectedCampaigns.data());
-				}
+			listItem.on('change', function() {
+				self.selectCampaign();
 			});
 		});
 	},
 
-	'makeGraph': function (data) {
+	'selectCampaign': function() {
+		var self = this,
+			selectedCampaigns = d3.selectAll('#campaign-list input:checked'),
+			selectedCampaignsLength = selectedCampaigns[0].length;
+
+		// clear graph and exit if not enough campaigns selected
+		if (selectedCampaignsLength < self.config.minCampaigns) {
+			self.clearGraph();
+			return;
+		}
+
+		self.drawGraph(selectedCampaigns.data());
+
+		// disable checkboxes and exit if too many campaigns selected
+		if (selectedCampaignsLength >= self.config.maxCampaigns) {
+			d3.selectAll('#campaign-list input:not(:checked)').attr('disabled', true);
+			return;
+		}
+
+		// enable all checkboxes
+		d3.selectAll('#campaign-list input')
+			.attr('disabled', null);
+	},
+
+	'drawGraph': function (data) {
 		var self = this,
 			campaigns,
 			chartItem,
-			metricSums = d3.map({"offers": 0, "shares": 0, "landings": 0, "leads": 0, "purchases": 0, "friends": 0});
+			metricSums = d3.map({'offers': 0, 'shares': 0, 'landings': 0, 'leads': 0, 'purchases': 0, 'friends': 0});
 
 		// calculate metric sums for selected campaigns
 		data.forEach(function(campaign) {
@@ -106,28 +127,34 @@ var floctory = {
 		}));
 
 		// clear previously created svg paths
-		self.chartArea.selectAll("path").remove();
-		self.chartYAxis.selectAll("g").remove();
+		self.clearGraph();
 
-		chartItem = self.chartArea.selectAll("path").data(campaigns, function(d) { return d.id; });
+		// bind campaign data and SVG paths
+		chartItem = self.chartArea.selectAll('path').data(campaigns, function(d) { return d.id; });
 
-		chartItem.enter().append("path")
-			.attr("d", function (d) { return self.area(d.values); })
-			.style("fill", function(d) { return self.color(d.id); });
+		// draw main chart
+		chartItem.enter().append('path')
+			.attr('d', function (d) { return self.area(d.values); })
+			.style('fill', function(d) { return self.color(d.id); });
 
 		// set metric sums as axis labels
 		self.yAxis.tickFormat(function (d) {
-			return d + ": " + metricSums.get(d);
+			return d + ': ' + metricSums.get(d);
 		});
 
 		// draw axis
 		self.chartYAxis
 			.call(self.yAxis)
-			.selectAll("text")
-				.attr("y", -5)
-				.attr("x", 10)
-				.attr("transform", "rotate(-90)")
-				.style("text-anchor", "start");
+			.selectAll('text')
+				.attr('y', -5)
+				.attr('x', 10)
+				.attr('transform', 'rotate(-90)')
+				.style('text-anchor', 'start');
+	},
+
+	'clearGraph': function() {
+		this.chartArea.selectAll('path').remove();
+		this.chartYAxis.selectAll('path, g').remove();
 	}
 }
 
